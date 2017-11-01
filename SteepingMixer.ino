83caaadd27567bf58cf09107701b0182cfc4f206
sprenkle <david@sprenkle.net>
2017-10-31 22:09:11 -0500

    Initial Checkin

---
+++ SteepingMixer.ino
@@ -0,0 +1,102 @@
+int Pin0 = 8; 
+int Pin1 = 9; 
+int Pin2 = 10; 
+int Pin3 = 11; 
+int _step = 0; 
+boolean dir = true;
+unsigned long LONG_DELAY_MS = 60000; // 1000 is one second times the number of seconds 60: 1000 * 60 = 60000 once per minute.  
+int angle = 682; // 682 is roughly 60 degrees increase or decrease to change how much it moves
+
+void setup() 
+{ 
+  pinMode(Pin0, OUTPUT);  
+  pinMode(Pin1, OUTPUT);  
+  pinMode(Pin2, OUTPUT);  
+  pinMode(Pin3, OUTPUT);  
+}
+ 
+void loop() 
+{ 
+  for(int i = 0; i < angle; i++){
+    step();
+  }
+  digitalWrite(Pin0, LOW);  
+  digitalWrite(Pin1, LOW); 
+  digitalWrite(Pin2, LOW); 
+  digitalWrite(Pin3, LOW); 
+  delay(LONG_DELAY_MS);
+//  unsigned long startMillis = millis();
+//  while (startMillis + LONG_DELAY_MS > millis() && millis() >= startMillis);
+}
+
+void step(){
+ switch(_step){ 
+   case 0: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, HIGH); 
+   break;  
+   case 1: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, HIGH); 
+   break;  
+   case 2: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+   case 3: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, HIGH); 
+     digitalWrite(Pin2, HIGH); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+   case 4: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, HIGH); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+   case 5: 
+     digitalWrite(Pin0, HIGH);  
+     digitalWrite(Pin1, HIGH); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+     case 6: 
+     digitalWrite(Pin0, HIGH);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+   case 7: 
+     digitalWrite(Pin0, HIGH);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, HIGH); 
+   break;  
+   default: 
+     digitalWrite(Pin0, LOW);  
+     digitalWrite(Pin1, LOW); 
+     digitalWrite(Pin2, LOW); 
+     digitalWrite(Pin3, LOW); 
+   break;  
+ } 
+ if(dir){ 
+   _step++; 
+ }else{ 
+   _step--; 
+ } 
+ if(_step>7){ 
+   _step=0; 
+ } 
+ if(_step<0){ 
+   _step=7; 
+ } 
+ delay(1); 
+}
+
